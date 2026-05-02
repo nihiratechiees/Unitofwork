fcc0e285bb72eedd6e23ecf4ed3016d1ff8a329b
nihiratechiees <nihiratechiees@gmail.com>
2026-05-02 11:45:08 +0530

    added swagger

--- Program.cs
+++ Program.cs
@@ -11,13 +11,16 @@ builder.Services.AddDbContext<TestDBContext>(options=>
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi(); 
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
